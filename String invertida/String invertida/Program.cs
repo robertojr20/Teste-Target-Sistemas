ddcd8b0ce25ec88047abc6cb25d2e06fd8e9c7c9
Roberto Varela Dos Santos  Junior <83435209+robertojr20@users.noreply.github.com>
2023-03-15 18:16:01 -0300

    Add files via upload

---
+++ String invertida/String invertida/Program.cs
@@ -0,0 +1,28 @@
+﻿/*5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente 
+   definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;*/
+
+
+using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite uma string: ");
+        string str = Console.ReadLine();
+
+        string invertedStr = "";
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            invertedStr += str[i];
+        }
+
+        Console.WriteLine("A string invertida é: " + invertedStr);
+    }
+}
+
